7cb71e8b8f70290bccbbd103646bed4450b274d5
Nelson de Pontes Tobias <nelson.tobias@gazin.com.br>
2020-11-02 19:03:57 -0300

    [fix] Tentando deixar a estrutura de pastas para usar no serveless da vercel

--- api/link.js
+++ api/link.js
@@ -1,7 +1,27 @@
 const axios = require('axios');
 const cheerio = require('cheerio')
 
-module.exports = async (req, res) => {
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+
+
+const link = async (req, res) => {
   const filter = req.headers.url
 
   try {
@@ -30,3 +50,5 @@ module.exports = async (req, res) => {
     })
   }
 }
+
+module.exports = allowCors(link)
